6bd4d9af857d018f0a441cf8f22f146664f4c08e
builttwospill <builttwospill@users.noreply.github.com>
2014-04-29 23:39:59 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+?>
+
+<html>
+<head>
+	<title>Home Automation</title>
+	<link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
+	<style type="text/css">
+		table {border-width: 1; border: solid; border-color: white}
+		h1 {color: #FED47D; font-family: Audiowide}
+		h3 {color: #A14141; font-family: Audiowide}
+	</style>
+</head>
+<body bgcolor="#8B87FF"> <!-- #FED47D -->
+	<center>
+		<!-- <h1>Home Automation</h1><br /> -->
+		<iframe src="https://docs.google.com/presentation/d/17FbAiLuroq4zwJ5KxMFngWdV2kDPfTIjYyPZwGJFfdU/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+		<table>
+			<tr>
+				<td>
+					<a href="http://builttwospill.mynetgear.com:8088" target="_blank">
+						<h1>Control Center</h1>
+					</a>
+				</td>
+				<!-- <td>
+					<a href="http://builttwospill.mynetgear.com:8088">
+						<img src="/new/homeauto.png" height="100px">
+					</a>
+				</td> -->
+			</tr>
+			<tr>
+				<td>
+					<a href="http://builttwospill.mynetgear.com:8081" target="_blank">
+						<h1><center>Video Feed</center></h1>
+					</a>
+				</td>
+				<!-- <td>
+					<a href="http://builttwospill.mynetgear.com:8081">
+						<img src="/new/videofeed.png" height="100px">
+					</a>
+				</td> -->
+			</tr>
+			<tr>
+				<td>
+					<a href="https://github.com/builttwospill/AutoHome/blob/master/OS4.ino" target="_blank">
+						<h1><center>Source Code</center></h1>
+					</a>
+				</td>	
+			</tr>
+		</table>
+		<h3>(this page was served from a raspberry pi)</h3>
+	</center>
+</body>
+</html>
